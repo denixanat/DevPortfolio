5958c1d503ee5a99d50417b02f581bf04017878a
Xanat <denixanatgarciahernandez@gmail.com>
2023-10-11 17:22:00 -0600

    components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from '../../pages/home/home.component';
+import { AboutComponent } from '../../pages/about/about.component';
 
 @NgModule({
   declarations: [
