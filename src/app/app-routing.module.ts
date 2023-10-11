91211b34e74b7ce539cebe316be67dbcc3fd31ec
Xanat <denixanatgarciahernandez@gmail.com>
2023-10-11 17:34:51 -0600

    routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from 'pages/about/about.component';
 import { HomeComponent } from 'pages/home/home.component';
 
 const routes: Routes = [
-{path: "none", component: HomeComponent},
+{path: "home", component: HomeComponent},
 {path: "about", component: AboutComponent}
 ];
 
