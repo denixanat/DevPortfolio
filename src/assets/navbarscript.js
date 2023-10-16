8331716216074bf27c6770e9a12c91d5487c0148
denixanat <denixanatdev.gmail.com>
2023-10-15 19:39:52 -0600

    testing

---
+++ src/assets/navbarscript.js
@@ -0,0 +1,108 @@
+
+
+const carouselText = [
+	{text: "visión", color: "white"},
+	{text: "código", color: "white"},
+	{text: "portafolio", color: "white"}
+  ]
+  
+  $( document ).ready(async function() {
+	carousel(carouselText, "#feature-text")
+  });
+  
+  async function typeSentence(sentence, eleRef, delay = 100) {
+	const letters = sentence.split("");
+	let i = 0;
+	while(i < letters.length) {
+	  await waitForMs(delay);
+	  $(eleRef).append(letters[i]);
+	  i++
+	}
+	return;
+  }
+  
+  async function deleteSentence(eleRef) {
+	const sentence = $(eleRef).html();
+	const letters = sentence.split("");
+	let i = 0;
+	while(letters.length > 0) {
+	  await waitForMs(100);
+	  letters.pop();
+	  $(eleRef).html(letters.join(""));
+	}
+  }
+  
+  async function carousel(carouselList, eleRef) {
+	  var i = 0;
+	  while(true) {
+		updateFontColor(eleRef, carouselList[i].color)
+		await typeSentence(carouselList[i].text, eleRef);
+		await waitForMs(1500);
+		await deleteSentence(eleRef);
+		await waitForMs(500);
+		i++
+		if(i >= carouselList.length) {i = 0;}
+	  }
+  }
+  
+  function updateFontColor(eleRef, color) {
+	$(eleRef).css('color', color);
+  }
+  
+  function waitForMs(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms))
+  }
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
