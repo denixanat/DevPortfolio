0d9e43aa4086d1fc277ea62f4634f4e9e6c78d3b
denixanat <denixanatdev.gmail.com>
2023-10-16 20:49:18 -0600

    explorar router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from 'pages/about/about.component';
 import { HomeComponent } from 'pages/home/home.component';
+import { ExplorarComponent } from './explorar/explorar.component';
 
 const routes: Routes = [
 {path: "home", component: HomeComponent},
-{path: "about", component: AboutComponent}
+{path: "about", component: AboutComponent},
+{path: "explorar", component: ExplorarComponent}
 ];
 
 @NgModule({
